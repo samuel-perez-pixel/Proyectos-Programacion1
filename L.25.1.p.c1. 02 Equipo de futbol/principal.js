a5a2aab31dc4d1e41c306adcc20da6644c881225
samuel-perez-pixel <samuelperez6195@gmail.com>
2025-02-02 15:46:17 -0400

    Cambio

--- L.25.1.p.c1. 02 Equipo de futbol/principal.js
+++ L.25.1.p.c1. 02 Equipo de futbol/principal.js
@@ -2,7 +2,7 @@
 Conociendo los resultados de varios juegos de tu equipo de fútbol, donde 1 es ganar y 0 es 
 perder, hacer un programa que indique el porcentaje de juegos que ganaste. 
 Ej. Resultados de 6 juegos: 1, 1, 0, 1, 0, 1, 1 
-la salida requerida presenta el siguiente formato: */
+la salida requerida presenta el siguiente formato:Ganaste el 66.67% de los juegos  */
 import Cl_Equipo from "./Cl_Equipo.js";
 import Cl_Juego from "./Cl_Juego.js";
 
